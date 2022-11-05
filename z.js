0c5180a32947fe1c061093102546c1a3066b6830
Sasorii <42864565+imSasori4@users.noreply.github.com>
2022-11-06 07:48:00 +0800

    Add files via upload

---
+++ z.js
@@ -0,0 +1,47 @@
+ function changeStyle() {
+    const element = document.getElementById("Ziv");
+    element.style.color = "red";
+  
+  
+  
+ }
+function change() {
+    const element = document.getElementById("Viz");
+    element.style.color = "blue";
+  
+}
+  
+function mad() {
+  document.getElementById("carding").src= "mad.png";
+  document.getElementById("madx").innerHTML = "Madara God";
+  document.getElementById("hue").innerHTML = "(うちはマダラ, Uchiha Madara) was the legendary leader of the Uchiha clan.";
+  
+  
+}
+
+function itc() {
+  document.getElementById("hihi").src= "itachz.png";
+  document.getElementById("hihiz").innerHTML = "Itachi God";
+  document.getElementById("ngez").innerHTML = "Uchiha Itachi was a shinobi of Konohagakure's Uchiha clan who served as an Anbu Captain. He later became an international criminal after murdering his entire clan";
+}
+function sask() {
+  document.getElementById("sass").src = "sask.png";
+  document.getElementById("sassi").innerHTML = "Mangekyou";
+   document.getElementById("kez").innerHTML = "Sasuke Uchiha (うちはサスケ, Uchiha Sasuke) is one of the last surviving members of Konohagakure's Uchiha clan";
+}
+
+function zag() {
+  document.getElementById("sag").src = "sage.png";
+  document.getElementById("sagx").innerHTML = "SaGe Mode";
+  document.getElementById("hex").innerHTML = "(うずまきナルト, Uzumaki Naruto) is a shinobi of Konohagakure's Uzumaki clan. He became the jinchūriki of the Nine-Tails";
+}
+function kush() {
+  document.getElementById("zi").src = "kushii.jpg";
+  document.getElementById("ex").innerHTML = "Kushina God";
+  document.getElementById("huuz").innerHTML = "(うずまきクシナ, Uzumaki Kushina) was a Konohagakure kunoichi who originated from Uzushiogakure's Uzumaki clan";
+}
+function minat() {
+  document.getElementById("zix").src = "mina.png";
+  document.getElementById("exx").innerHTML = "Sage Mode";
+  document.getElementById("uz").innerHTML = "(波風ミナト, Namikaze Minato) was the Fourth Hokage (四代目火影, Yondaime Hokage";
+}
